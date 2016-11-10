471075be02c19d9da6b789433fc741f9b5fbf151
h3h3 <h3h3sharp@gmail.com>
2016-11-10 01:09:58 +0100

    added async support

---
+++ Threading/GameDispatcher.cs
@@ -0,0 +1,83 @@
+﻿// <copyright file="GameDispatcher.cs" company="EnsageSharp">
+//    Copyright (c) 2016 EnsageSharp.
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see http://www.gnu.org/licenses/
+// </copyright>
+namespace Ensage.Common.Threading
+{
+    using System;
+    using System.Threading;
+
+    public static class GameDispatcher
+    {
+        #region Constructors and Destructors
+
+        static GameDispatcher()
+        {
+            Game.OnUpdate += UpdateDispatcher;
+            Game.OnIngameUpdate += IngameUpdateDispatcher;
+        }
+
+        #endregion
+
+        #region Public Events
+
+        public static event GameIngameUpdate OnIngameUpdate;
+
+        public static event GameUpdate OnUpdate;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///     Schedules <paramref name="action" /> to be executed on next Update
+        /// </summary>
+        /// <param name="action"></param>
+        public static void BeginInvoke(Action action)
+        {
+            GameSynchronizationContext.Instance.Post(state => action(), null);
+        }
+
+        #endregion
+
+        #region Methods
+
+        internal static void DispatchAction(Action action)
+        {
+            var gameContext = GameSynchronizationContext.Instance;
+            var context = SynchronizationContext.Current;
+
+            try
+            {
+                SynchronizationContext.SetSynchronizationContext(gameContext);
+                action();
+                gameContext.RunOnCurrentThread();
+            }
+            finally
+            {
+                SynchronizationContext.SetSynchronizationContext(context);
+            }
+        }
+
+        private static void IngameUpdateDispatcher(EventArgs args)
+        {
+            DispatchAction(() => OnIngameUpdate?.Invoke(EventArgs.Empty));
+        }
+
+        private static void UpdateDispatcher(EventArgs args)
+        {
+            DispatchAction(() => OnUpdate?.Invoke(EventArgs.Empty));
+        }
+
+        #endregion
+    }
+}
---
+++ Threading/GameSynchronizationContext.cs
@@ -0,0 +1,88 @@
+﻿// <copyright file="GameSynchronizationContext.cs" company="EnsageSharp">
+//    Copyright (c) 2016 EnsageSharp.
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see http://www.gnu.org/licenses/
+// </copyright>
+namespace Ensage.Common.Threading
+{
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    public class GameSynchronizationContext : SynchronizationContext
+    {
+        #region Static Fields
+
+        private static readonly object SyncRoot = new object();
+
+        private static GameSynchronizationContext instance;
+
+        #endregion
+
+        #region Fields
+
+        private readonly ConcurrentQueue<KeyValuePair<SendOrPostCallback, object>> queue =
+            new ConcurrentQueue<KeyValuePair<SendOrPostCallback, object>>();
+
+        #endregion
+
+        #region Public Properties
+
+        public static GameSynchronizationContext Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (SyncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new GameSynchronizationContext();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public override SynchronizationContext CreateCopy()
+        {
+            return new GameSynchronizationContext();
+        }
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            this.queue.Enqueue(new KeyValuePair<SendOrPostCallback, object>(d, state));
+        }
+
+        #endregion
+
+        #region Methods
+
+        internal void RunOnCurrentThread()
+        {
+            KeyValuePair<SendOrPostCallback, object> workItem;
+
+            while (!this.queue.IsEmpty && this.queue.TryDequeue(out workItem))
+            {
+                workItem.Key(workItem.Value);
+            }
+        }
+
+        #endregion
+    }
+}
