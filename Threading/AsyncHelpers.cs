b6c7e7664d9f5d50a9c86a6ffff83dcca9bae6f6
h3h3 <h3h3sharp@gmail.com>
2016-11-11 21:23:36 +0100

    fixed formatting and improved Combo

--- Threading/AsyncHelpers.cs
+++ Threading/AsyncHelpers.cs
@@ -1,4 +1,17 @@
-﻿namespace Ensage.Common.Threading
+﻿// <copyright file="AsyncHelpers.cs" company="EnsageSharp">
+//    Copyright (c) 2016 EnsageSharp.
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see http://www.gnu.org/licenses/
+// </copyright>
+namespace Ensage.Common.Threading
 {
     using System;
     using System.Threading;
@@ -7,44 +20,25 @@
     using Ensage.Common.Extensions;
 
     /// <summary>
-    /// ...
+    ///     Game Async helper functions.
     /// </summary>
     public static class AsyncHelpers
     {
         #region Public Methods and Operators
 
         /// <summary>
-        /// Waits until the target has a certain modifier.
-        /// </summary>
-        /// <param name="target"></param>
-        /// <param name="name">Name of the modifier.</param>
-        /// <param name="ct"></param>
-        /// <returns></returns>
-        public static async Task<bool> WaitModifierAsync(this Unit target, string name,
-            CancellationToken ct = default(CancellationToken))
-        {
-            try
-            {
-                while (!target.HasModifier(name))
-                    await Task.Delay(100, ct);
-            }
-            catch (OperationCanceledException)
-            {
-                return false;
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// Move to a target until you're in a certain range and stops then.
+        ///     Move to a target until you're in a certain range and stops then.
         /// </summary>
         /// <param name="me"></param>
         /// <param name="target"></param>
         /// <param name="range"></param>
         /// <param name="ct"></param>
         /// <returns></returns>
-        public static async Task<bool> MoveToTargetAsync(this Unit me, Unit target, float range,
-              CancellationToken ct = default(CancellationToken))
+        public static async Task<bool> MoveToTargetAsync(
+            this Unit me,
+            Unit target,
+            float range,
+            CancellationToken ct = default(CancellationToken))
         {
             try
             {
@@ -57,10 +51,40 @@
             }
             catch (OperationCanceledException)
             {
-                me.Stop();
                 return false;
             }
-            me.Stop();
+            finally
+            {
+                me.Stop();
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        ///     Waits until the target has a certain modifier.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="name">Name of the modifier.</param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        public static async Task<bool> WaitModifierAsync(
+            this Unit target,
+            string name,
+            CancellationToken ct = default(CancellationToken))
+        {
+            try
+            {
+                while (!target.HasModifier(name))
+                {
+                    await Task.Delay(100, ct);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                return false;
+            }
+
             return true;
         }
 
--- Threading/Combo.cs
+++ Threading/Combo.cs
@@ -1,29 +1,59 @@
-﻿using System;
-using System.Threading.Tasks;
-
+﻿// <copyright file="Combo.cs" company="EnsageSharp">
+//    Copyright (c) 2016 EnsageSharp.
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see http://www.gnu.org/licenses/
+// </copyright>
 namespace Ensage.Common.Threading
 {
+    using System;
+    using System.Diagnostics.CodeAnalysis;
     using System.Threading;
+    using System.Threading.Tasks;
     using System.Windows.Input;
 
     /// <summary>
-    /// Executing a combo with async functions
+    ///     Executing a combo with async functions
     /// </summary>
-    public class Combo
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "StyleCop.SA1310")]
+    public class Combo : IDisposable
     {
-        // ReSharper disable once InconsistentNaming
+        #region Constants
+
         private const uint WM_KEYUP = 0x0101;
-        // ReSharper disable once InconsistentNaming
+
         private const uint WM_SYSKEYUP = 0x0105;
 
+        #endregion
+
+        #region Fields
+
         private readonly Func<CancellationToken, Task> comboFunction;
+
         private readonly Key key;
+
+        private readonly ulong virtualKey;
+
         private Task currentExecution;
+
+        private bool disposed;
 
         private CancellationTokenSource token;
 
+        #endregion
+
+        #region Constructors and Destructors
+
         /// <summary>
-        /// Creates a combo.
+        ///     Creates a combo.
         /// </summary>
         /// <param name="comboFunction">This function will be executed while the key is pressed.</param>
         /// <param name="key">While this key is pressed your combofunction will be executed.</param>
@@ -31,52 +61,128 @@ namespace Ensage.Common.Threading
         {
             this.comboFunction = comboFunction;
             this.key = key;
-            Game.OnWndProc += this.Game_OnWndProc;
+            this.virtualKey = (ulong)KeyInterop.VirtualKeyFromKey(key);
         }
 
-        private void Game_OnWndProc(WndEventArgs args)
-        {
-            if (this.currentExecution == null)
-                return;
+        #endregion
 
-            if (((args.Msg == WM_KEYUP) || (args.Msg == WM_SYSKEYUP)) &&
-                ((int)args.WParam == KeyInterop.VirtualKeyFromKey(this.key)))
+        #region Public Properties
+
+        /// <summary>
+        ///     Returns true if the current execution is completed.
+        /// </summary>
+        public bool IsCompleted
+        {
+            get
             {
-                this.token?.Cancel();
+                return this.currentExecution != null && this.currentExecution.IsCompleted;
             }
         }
 
+        #endregion
+
+        #region Public Methods and Operators
+
         /// <summary>
-        /// Cancels the execution of the current combo.
+        ///     Cancels the execution of the current combo.
         /// </summary>
         public void Cancel()
         {
-            this.token?.Cancel();
+            if (this.token == null)
+            {
+                return;
+            }
+
+            if (!this.token.IsCancellationRequested)
+            {
+                this.token.Cancel();
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
         /// <summary>
-        /// Executes your combo until it's either finished or canceled.
+        ///     Executes your combo until it's either finished or canceled.
         /// </summary>
         /// <returns></returns>
         public async Task Execute()
         {
             if (!Game.IsKeyDown(this.key))
+            {
                 return;
+            }
 
-            if ((this.currentExecution != null) && !this.currentExecution.IsCompleted)
+            if (!this.IsCompleted)
+            {
                 return;
+            }
 
             try
             {
-                this.token = new CancellationTokenSource();
-                this.currentExecution = this.comboFunction(this.token.Token);
+                this.Prepare();
                 await this.currentExecution;
-                this.currentExecution = null;
             }
             catch (OperationCanceledException)
             {
-                this.currentExecution = null;
+                // canceled
+            }
+            finally
+            {
+                this.Finish();
             }
         }
+
+        #endregion
+
+        #region Methods
+
+        private void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                this.Finish();
+            }
+
+            this.disposed = true;
+        }
+
+        private void Finish()
+        {
+            this.Cancel();
+
+            Game.OnWndProc -= this.Game_OnWndProc;
+            this.currentExecution = null;
+        }
+
+        private void Game_OnWndProc(WndEventArgs args)
+        {
+            if (this.currentExecution == null)
+            {
+                return;
+            }
+
+            if (((args.Msg == WM_KEYUP) || (args.Msg == WM_SYSKEYUP)) && (args.WParam == this.virtualKey))
+            {
+                this.Cancel();
+            }
+        }
+
+        private void Prepare()
+        {
+            Game.OnWndProc += this.Game_OnWndProc;
+            this.token = new CancellationTokenSource();
+            this.currentExecution = this.comboFunction(this.token.Token);
+        }
+
+        #endregion
     }
 }
