2cb1c0af09dbaa34f25528c0cb24ee7bccf97210
Rulfy <git@zynox.net>
2016-11-11 20:31:17 +0100

    added async helpers and stuff

---
+++ Threading/AsyncHelpers.cs
@@ -0,0 +1,69 @@
+﻿namespace Ensage.Common.Threading
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Ensage.Common.Extensions;
+
+    /// <summary>
+    /// ...
+    /// </summary>
+    public static class AsyncHelpers
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Waits until the target has a certain modifier.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="name">Name of the modifier.</param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        public static async Task<bool> WaitModifierAsync(Unit target, string name,
+            CancellationToken ct = default(CancellationToken))
+        {
+            try
+            {
+                while (!target.HasModifier(name))
+                    await Task.Delay(100, ct);
+            }
+            catch (OperationCanceledException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Move to a target until you're in a certain range and stops then.
+        /// </summary>
+        /// <param name="me"></param>
+        /// <param name="target"></param>
+        /// <param name="range"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        public static async Task<bool> MoveToTargetAsync(Unit me, Unit target, float range,
+              CancellationToken ct = default(CancellationToken))
+        {
+            try
+            {
+                while (me.Distance2D(target) > range)
+                {
+                    me.Move(target.Position);
+
+                    await Task.Delay(100, ct);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                me.Stop();
+                return false;
+            }
+            me.Stop();
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ Threading/Combo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Ensage.Common.Threading
+{
+    using System.Threading;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// Executing a combo with async functions
+    /// </summary>
+    public class Combo
+    {
+        // ReSharper disable once InconsistentNaming
+        private const uint WM_KEYUP = 0x0101;
+        // ReSharper disable once InconsistentNaming
+        private const uint WM_SYSKEYUP = 0x0105;
+
+        private readonly Func<CancellationToken, Task> comboFunction;
+        private readonly Key key;
+        private Task currentExecution;
+
+        private CancellationTokenSource token;
+
+        /// <summary>
+        /// Creates a combo.
+        /// </summary>
+        /// <param name="comboFunction">This function will be executed while the key is pressed.</param>
+        /// <param name="key">While this key is pressed your combofunction will be executed.</param>
+        public Combo(Func<CancellationToken, Task> comboFunction, Key key)
+        {
+            this.comboFunction = comboFunction;
+            this.key = key;
+            Game.OnWndProc += this.Game_OnWndProc;
+        }
+
+        private void Game_OnWndProc(WndEventArgs args)
+        {
+            if (this.currentExecution == null)
+                return;
+
+            if (((args.Msg == WM_KEYUP) || (args.Msg == WM_SYSKEYUP)) &&
+                ((int)args.WParam == KeyInterop.VirtualKeyFromKey(this.key)))
+            {
+                this.token?.Cancel();
+            }
+        }
+
+        /// <summary>
+        /// Executes your combo until it's either finished or canceled.
+        /// </summary>
+        /// <returns></returns>
+        public async Task Execute()
+        {
+            if (!Game.IsKeyDown(this.key))
+                return;
+
+            if ((this.currentExecution != null) && !this.currentExecution.IsCompleted)
+                return;
+
+            try
+            {
+                this.token = new CancellationTokenSource();
+                this.currentExecution = this.comboFunction(this.token.Token);
+                await this.currentExecution;
+                this.currentExecution = null;
+            }
+            catch (OperationCanceledException)
+            {
+                this.currentExecution = null;
+            }
+        }
+    }
+}
